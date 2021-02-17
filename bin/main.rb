9f821c58b4378adfa34983f81b8c87b83a544280
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-02-17 13:40:40 -0300

    Fix linter errors including empty spaces

--- bin/main.rb
+++ bin/main.rb
@@ -2,28 +2,32 @@
 name1=''
 marker1=''
 
-puts "Welcome Player 1, please introduce your name:"
+puts 'Welcome Player 1, please introduce your name:'
 name1 = gets.chomp
 
 loop do
-  puts "Please choose your marker: X or O"
+  puts 'Please choose your marker: X or O'
   marker1 = gets.chomp.upcase
-  break if (marker1 == 'O' || marker1 == 'X')
+  break if ['X', 'O'].include?(marker1)
 end
 
-puts "Welcome Player 2, please introduce your name:"
+puts 'Welcome Player 2, please introduce your name:'
 name2 = gets.chomp
 
-marker2 = ( marker1 == 'X' ? 'O' : 'X' )
+marker2 = (marker1 == 'X' ? 'O' : 'X')
 puts "You will play with #{marker2}"
 
 current_player = name1
 1.upto(9) do
-    puts "#{current_player} it's your turn. Choose a position"
-    position = gets.chomp
-    puts "#{current_player} chose position #{position}"
-
-    current_player = (current_player == name1 ? name2 : name1)
+  position = 0
+  puts "#{current_player} it\'s your turn. Choose a position"
+  loop do
+    position = gets.chomp.to_i
+    break if (1..9).include?(position)
+    puts 'Please choose a position from 1 to 9'
+  end
+  puts "#{current_player} chose position #{position}"
+  current_player = (current_player == name1 ? name2 : name1)
 end
 
 players = [name1, name2]
