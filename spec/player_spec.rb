112e8002cf04bfe5857a53befe1b3234c906aedd
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-02-22 16:41:44 -0400

    Replace variable assignments with let

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,16 +3,16 @@ require 'rspec'
 require './lib/player.rb'
 
 describe Player do
+  let(:player) { Player.new('Juan', :X) }
+
   describe "#to_s" do
     it "returns the name of the player" do
-      player = Player.new('Juan', :X)
       expect(player.to_s).to eql('Juan')
     end
   end
 
   describe "#opposite_marker" do
     it "returns the marker opposite to the player's marker" do
-      player = Player.new('Juan', :X)
       expect(player.opposite_marker).to eql(:O)
     end
   end
